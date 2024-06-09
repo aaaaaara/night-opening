932eabaada9649f6af0e509f69ddc854cff7d6d6
aaaaaara <araring.j@gmail.com>
2024-06-09 14:25:17 +0900

    core: install react-hook-form & change axios instance name

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
 
 const nightOpeningURL = 'test';
-export const nightOpeningAxios: AxiosInstance = axios.create({
+export const axiosInstance: AxiosInstance = axios.create({
   baseURL: `${nightOpeningURL}`,
 });
